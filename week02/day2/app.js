f4e7dc79650d7623b2ff497754a739700584b510
DaweUrbi <urban.da@icloud.com>
2022-09-26 10:52:38 -0700

    class w2d2

--- week02/day2/app.js
+++ week02/day2/app.js
@@ -26,4 +26,4 @@ app.get("/", (req, res) => {
   res.send("You are logged in");
 });
 
-app.listen("3000", console.log("Server is running on port 3000"));
+app.listen("3002", console.log("server running 3002"));
--- week02/day2/exercise.js
+++ week02/day2/exercise.js
@@ -2,6 +2,8 @@
 const cookieSession = require("cookie-session");
 const express = require("express");
 const bcrypt = require("bcrypt");
+const authRouter = require("./routes/auth");
+const userRouter = require("./routes/user");
 
 const app = express();
 
@@ -15,65 +17,7 @@ app.use(
   })
 );
 
-const users = {
-  test: {
-    name: "Test",
-    username: "test",
-    password: "$2b$12$4rG6Hrijl8VYWVIMEORyruKrudrkviq2Kcbd9RxLZkWXizJPxXOXK",
-  },
-};
+app.use("/auth", authRouter);
+app.use("/user", userRouter);
 
-const hashPassword = async (password, saltRounds) => {
-  const salt = await bcrypt.genSalt(saltRounds);
-  const hash = await bcrypt.hash(password, salt);
-  // const hash = await bcrypt.hash(password, saltRounds);
-  return hash;
-};
-
-app.get("/register", (req, res) => {
-  res.render("register");
-});
-
-app.post("/register", async (req, res) => {
-  const { name, username, password } = req.body;
-  console.log("username", username);
-  const hashedPassword = await hashPassword(password, 12);
-  users[username] = { name, username, password: hashedPassword };
-
-  console.log("users", users);
-  res.cookie("username", username);
-  res.send("user created");
-});
-
-app.get("/login", (req, res) => {
-  res.render("login");
-});
-
-app.post("/login", async (req, res) => {
-  const receivedUsername = req.body.username;
-  const receivedPassword = req.body.password;
-  const user = users[receivedUsername];
-  if (!user) return res.send("invalid username");
-  const isMatch = await bcrypt.compare(receivedPassword, user.password);
-  if (isMatch) {
-    // res.cookie("username", user.username);
-    req.session.username = user.username;
-    return res.send("You are logged in");
-  }
-  res.send("invalid password");
-});
-
-app.get("/profile", (req, res) => {
-  const username = req.session.username;
-  if (!username) return res.redirect("/login");
-  const user = users[username];
-  res.render("profile", { username: user.username, password: user.password });
-});
-
-app.post("/logout", (req, res) => {
-  // res.clearCookie("username");
-  req.session = null;
-  res.redirect("/login");
-});
-
-app.listen(8000, () => console.log("Server is running on port: 8000"));
+app.listen(8080, () => console.log("server running 8080"));
